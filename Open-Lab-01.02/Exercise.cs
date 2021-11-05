d886a8ba920de47fa6f3fb95b10050759c90adee
Daniel Pitek <Daniel813245@outlook.sk>
2021-11-05 20:26:07 +0100

    vyriesene

--- Open-Lab-01.02/Exercise.cs
+++ Open-Lab-01.02/Exercise.cs
@@ -4,9 +4,10 @@ namespace Open_Lab_01._02
 {
     public class Exercise
     {
+        
         public int Sum(int a, int b)
         {
-            throw new NotImplementedException();
+            return a + b;
         }
     }
 }
